4e462847e6e0db2081410c23f84fdc0c4d3fd2d3
Filippo Squillace <squillace.filippo@gmail.com>
2019-04-12 17:57:09 +1000

    First commit

---
+++ pearl-config/install.sh
@@ -0,0 +1,83 @@
+function post_install(){
+    local pluginname=zenburn
+    local giturl=https://github.com/jnurmine/Zenburn.git
+
+    info "Installing or updating the $pluginname git repository..."
+    local plugin_root="${PEARL_PKGVARDIR}/plugins/pack/pearl/start"
+    mkdir -p "$plugin_root"
+    install_or_update_git_repo $giturl "$plugin_root/$pluginname" master
+    [[ -e "$plugin_root/$pluginname/doc" ]] && \
+        vim -c "helptags $plugin_root/$pluginname/doc" -c q
+
+
+    local pluginname=nord
+    local giturl=https://github.com/arcticicestudio/nord-vim.git
+
+    info "Installing or updating the $pluginname git repository..."
+    local plugin_root="${PEARL_PKGVARDIR}/plugins/pack/pearl/start"
+    mkdir -p "$plugin_root"
+    install_or_update_git_repo $giturl "$plugin_root/$pluginname" master
+    [[ -e "$plugin_root/$pluginname/doc" ]] && \
+        vim -c "helptags $plugin_root/$pluginname/doc" -c q
+
+
+    local pluginname=dracula
+    local giturl=https://github.com/dracula/vim.git
+
+    info "Installing or updating the $pluginname git repository..."
+    local plugin_root="${PEARL_PKGVARDIR}/plugins/pack/pearl/start"
+    mkdir -p "$plugin_root"
+    install_or_update_git_repo $giturl "$plugin_root/$pluginname" master
+    [[ -e "$plugin_root/$pluginname/doc" ]] && \
+        vim -c "helptags $plugin_root/$pluginname/doc" -c q
+
+
+    local pluginname=solarized
+    local giturl=https://github.com/altercation/vim-colors-solarized.git
+
+    info "Installing or updating the $pluginname git repository..."
+    local plugin_root="${PEARL_PKGVARDIR}/plugins/pack/pearl/start"
+    mkdir -p "$plugin_root"
+    install_or_update_git_repo $giturl "$plugin_root/$pluginname" master
+    [[ -e "$plugin_root/$pluginname/doc" ]] && \
+        vim -c "helptags $plugin_root/$pluginname/doc" -c q
+
+    setup_configuration "${PEARL_PKGVARDIR}/vimrc" \
+        _new_vimrc _apply_vimrc _unapply_vimrc
+}
+
+function post_update(){
+    post_install
+}
+
+function pre_remove(){
+    _unapply_vimrc
+    rm -rf "${PEARL_PKGVARDIR}/plugins"
+}
+
+_new_vimrc() {
+    local themes=()
+    local thm
+    for thm in "$PEARL_PKGDIR/"vimrc-theme-*
+    do
+        themes+=($(basename "${thm/vimrc-theme-/}"))
+    done
+
+    local theme=$(choose "Which theme would you like?" "zenburn" "${themes[@]}")
+    echo "set packpath+=$PEARL_PKGVARDIR/plugins" > "$PEARL_PKGVARDIR/vimrc"
+    cat "$PEARL_PKGDIR/vimrc-theme-$theme" >> "$PEARL_PKGVARDIR/vimrc"
+    return 0
+}
+
+_apply_vimrc() {
+    link vim "$PEARL_PKGVARDIR/vimrc"
+
+    return 0
+}
+
+_unapply_vimrc() {
+    unlink vim "$PEARL_PKGVARDIR/vimrc"
+
+    return 0
+}
+
---
+++ vimrc-theme-solarized
@@ -0,0 +1,11 @@
+
+let g:solarized_contrast = "low"
+let g:solarized_visibility= "normal"
+
+if has('gui_running')
+    set background=light
+else
+    set background=dark
+endif
+
+colorscheme solarized
